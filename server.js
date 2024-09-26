331f0939cf06548052293c94aeb72f24e2ab8692
qaltx <67969197+notqaltx@users.noreply.github.com>
2024-09-27 00:04:14 +0300

    Updated build.

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
